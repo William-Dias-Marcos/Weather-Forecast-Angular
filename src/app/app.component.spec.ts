d64ff7f424a6d9c287d3220556e8a6b2b0e06a9a
William-Dias-Marcos <william.diasmarcos@gmail.com>
2024-06-18 22:35:36 -0300

    test: add tests for AppComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,25 +1,23 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { ClimateComponent } from './components/climate/climate.component';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'; // Import HttpClientModule
-import { provideHttpClientTesting } from '@angular/common/http/testing'; // Import HttpClientTestingModule
 
 describe('AppComponent', () => {
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-    declarations: [AppComponent, ClimateComponent],
-    imports: [HttpClientModule],
-    providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClientTesting()]
-}{
-    declarations: [AppComponent, ClimateComponent],
-    imports: [HttpClientTestingModule],
-    providers: [provideHttpClient(withInterceptorsFromDi())]
-}).compileComponents();
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 });
