566f0372bd2bf619446c42b96e455afd461c666b
William-Dias-Marcos <william.diasmarcos@gmail.com>
2025-01-06 07:33:17 -0300

    chore: added new api key

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   apiUrl: 'https://api.openweathermap.org/data/2.5',
-  apiKey: '76da943746cd6d004420e7c26d9f6a7c'
+  apiKey: '76da943746cd6d004420e7c26d9f6a7c',
+  apiKey2: '5796abbde9106b7da4febfae8c44c232',
 };
